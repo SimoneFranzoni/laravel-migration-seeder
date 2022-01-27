864b676065746efeff000d3a16c936b6f4f7207e
SimoneFranzoni <simofranzoni99@gmail.com>
2022-01-27 17:20:39 +0100

    aggiunta faker

--- database/seeds/TripsTableSeeder.php
+++ database/seeds/TripsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 use App\Trip;
@@ -10,17 +11,21 @@ class TripsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $new_trip = new Trip();
-        $new_trip->address = "via fasulla 0";
-        $new_trip->city = "Salerno";
-        $new_trip->state = "Italia";
-        $new_trip->number_of_night = 5;
-        $new_trip->number_of_person = 2;
-        $new_trip->description = "None";
-        $new_trip->price = 199;
-        $new_trip->city = true;
-        $new_trip->save();
-    }
+
+        for($i = 0; $i<100; $i++){
+            
+            $new_trip = new Trip();
+            $new_trip->address = $faker -> streetAddress();
+            $new_trip->city = $faker -> city();
+            $new_trip->state = $faker -> state();
+            $new_trip->number_of_night = $faker -> numberBetween(1, 30);
+            $new_trip->number_of_person = $faker -> numberBetween(1, 10);
+            $new_trip->description = $faker -> text(200);
+            $new_trip->price = $faker -> numberBetween(50, 10000);
+            $new_trip->save();
+        
+        }
+}
 }
